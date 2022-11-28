30862bbbe42e8b7247ac4709ffa7b35afc3cac8b
ruin350 <kkr583@naver.com>
2022-11-28 12:30:11 +0900

    kakaomap마무리

---
+++ kakaomap/js/my-map.js
@@ -0,0 +1,88 @@
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+
+//  ------------------------------- map -------------------------------------
+
+
+class MapService{
+    static #instance = null;
+    static getInstance(){
+        if(this.#instance == null){
+            this.#instance = new MapService();
+        }
+        return this.#instance;
+    }
+    load() {
+        console.log("121")
+        // 등록시 this. 필수
+        this.addSearchInputEvent();
+        this.addAsideToggleButtonEvent();
+    }
+
+
+    addSearchInputEvent(){
+        const searchInput = document.querySelector(".search-input");
+
+        // 클릭이 일어났을때 invisible-resent를 toggle시켜라
+        // toggle = on, off의 개념
+        searchInput.onclick = () => {
+            const searchRecent = document.querySelector(".search-resent");
+            searchRecent.classList.toggle("invisible-resent");
+        }
+    }
+    
+    addAsideToggleButtonEvent(){
+        const toggleButton = document.querySelector(".toggle-button")
+        toggleButton.onclick = () => {
+            const aside = document.querySelector("aside")
+            aside.classList.toggle("invisible-aside")
+            // contains = 포함하고있는지 확인
+            if(aside.classList.contains("invisible-aside")){
+                toggleButton.textContent = "▶";
+            }else {
+                toggleButton.textContent = "◀";
+            }
+        }
+    }
+
+    addMenuTabEvent() {
+        const mainMenuTab = document.querySelectorAll(".mainmenutab");
+
+        for(let i = 0; i < mainMenuTab.length; i++) {
+            mainMenuTab[i].onclick = () => {
+                for(let j = 0; j < mainMenuTab.length; j++) {
+                    mainMenuTab[j].classList.remove("tab-selected");
+                }
+                mainMenuTab[i].classList.add("tab-selected");
+
+                const searchBody = document.querySelectorAll(".search-body");
+                for(let j = 0; j < searchBody.length; j++) {
+                    searchBody[j].classList.add("invisible-body");
+                }
+                searchBody[i].classList.remove("invisible-body");
+            }
+        }
+    }
+}
+
+
+// window 객체에 onload 되면 {실행문}실행
+window.onload = () => {
+    MapService.getInstance().load();
+}
